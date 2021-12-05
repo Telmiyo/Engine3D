331123e5af62a631516d944c72bdf4f90b48a5ed
Telmiyo <iamtelmo@proton.me>
2021-12-05 19:46:49 +0100

    File Manager & File Created

---
+++ Engine3D/Core/FileManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#pragma once
+#include "Module.h"
+#include "File.h"
+
+#include "Assimp/include/mesh.h"
+#include <string>
+
+class ComponentMesh;
+struct aiScene;
+
+class FileManager : public Module
+{
+public:
+
+	FileManager(Application* app, bool start_enabled = true);
+
+	bool Init() override;
+	update_status Update(float dt) override;
+	bool CleanUp() override;
+
+	File* createFile(const aiMesh* m);
+	bool saveFile();
+	bool loadfile();
+
+};
