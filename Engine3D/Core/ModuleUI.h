0dc59e8385eb6212e8c927eab2298598587691c8
Oscar Cuatrecasas <oscar@batou.xyz>
2022-01-06 12:10:49 +0100

    Added module UI

---
+++ Engine3D/Core/ModuleUI.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "Light.h"
+
+class ModuleUI : public Module
+{
+public:
+	ModuleUI(Application* app, bool start_enabled = true);
+	~ModuleUI();
+
+	bool Init() override;
+	bool Start() override;
+	update_status PreUpdate(float dt) override;
+	update_status PostUpdate(float dt) override;
+	bool CleanUp() override;
+
+	void OnGui() override;
+	void OnLoad(const JSONReader& reader) override;
+	void OnSave(JSONWriter& writer) const override;
+
+public:
+};
