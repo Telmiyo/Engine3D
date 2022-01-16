b28268bb87ab9a629607d816a60192469dbfce14
Montuuh <dmontufo@gmail.com>
2022-01-16 15:05:31 +0100

    Component Image now serialized. New component image ongui features. Set WHITE_FALLBACK texture as a default texture for components image

--- Engine3D/Core/ComponentImage.cpp
+++ Engine3D/Core/ComponentImage.cpp
@@ -58,7 +58,13 @@ bool ComponentImage::Update(float dt)
 		glTexCoordPointer(2, GL_FLOAT, 0, NULL);
 	}
 
-	 glBindTexture(GL_TEXTURE_2D, textureId);
+	
+	if (textureId != 0) glBindTexture(GL_TEXTURE_2D, textureId);
+	else // Called once when created an object. Set white fallback as a default texture
+	{ 
+		glBindTexture(GL_TEXTURE_2D, App->textures->whiteFallback);
+		SetTexture(App->textures->textures.at("WHITE_BALLBACK"));
+	}
 
 	glPushMatrix();
 	glMultMatrixf(transform.Transposed().ptr());
@@ -95,9 +101,62 @@ void ComponentImage::SetTexture(const TextureObject& texture)
 void ComponentImage::OnGui()
 {
 	if (ImGui::CollapsingHeader("Image")) {
-		ImGui::Text("Texture");
-		ImGui::Image((ImTextureID)App->textures->checkers, ImVec2(100, 100), ImVec2(0, 1), ImVec2(1, 0));
-		ImGui::Image((ImTextureID)textureId, ImVec2(128, 128), ImVec2(0, 1), ImVec2(1, 0));
-
+		ImGui::Text("Texture: ");
+		ImGui::SameLine();
+		if (textureId == 0) // Supposedly there is no textureId = 0 in textures array
+		{
+			ImGui::Text("None");
+		}
+		else
+		{
+			ImGui::Text(textureName.c_str());
+			ImGui::Image((ImTextureID)textureId, ImVec2(128, 128), ImVec2(0, 1), ImVec2(1, 0));
+		}
+			
 	}
+}
+
+void ComponentImage::OnLoad(const JSONReader& reader)
+{
+	if (reader.HasMember("Texture name"))
+	{
+		const rapidjson::Value& itemTextureName = reader["Texture name"];
+		textureName = itemTextureName.GetString();
+	}
+	if (reader.HasMember("Size"))
+	{
+		const rapidjson::Value& itemSize = reader["Size"];
+		int width_ = 0;
+		int height_ = 0;
+		int i = 0;
+		for (rapidjson::Value::ConstValueIterator it = itemSize.Begin(); it != itemSize.End(); ++it)
+		{
+			if (i == 0) width_ = it->GetInt();
+			else if (i == 1) height_ = it->GetInt();
+			i++;
+		}
+		width = width_;
+		height = height_;
+	}
+	if (reader.HasMember("Texture ID"))
+	{
+		const rapidjson::Value& itemTextureId = reader["Texture ID"];
+		textureId = itemTextureId.GetInt();
+	}
+}
+
+void ComponentImage::OnSave(JSONWriter& writer) const
+{
+	writer.String("Image");
+	writer.StartObject();
+	writer.String("Texture name");
+	writer.String(textureName.c_str());
+	writer.String("Size");
+	writer.StartArray();
+	writer.Int(width);
+	writer.Int(height);
+	writer.EndArray();
+	writer.String("Texture ID");
+	writer.Int(textureId);
+	writer.EndObject();
 }
