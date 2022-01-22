5f85a4fb41294c996a28c68eda498f0e70d42446
Montuuh <dmontufo@gmail.com>
2022-01-22 21:48:05 +0100

    Added component text but not implemented much (just imgui). Fixed some checkbox load things.

---
+++ Engine3D/Core/ComponentText.cpp
@@ -0,0 +1,58 @@
+#include "ComponentText.h"
+
+ComponentText::ComponentText(GameObject* parent) : Component(parent)
+{
+	componentType = ComponentType::COMPONENT_TEXT;
+
+}
+
+ComponentText::~ComponentText()
+{
+
+}
+
+bool ComponentText::Update(float dt)
+{
+	return true;
+}
+
+void ComponentText::OnGui()
+{
+	if (ImGui::CollapsingHeader("Text")) 
+	{
+		
+		// Just for debugging inside the engine
+		if (text != nullptr)
+		{
+			ImGui::Text("DEBUG: ");
+			ImGui::SameLine();
+			ImGui::Text(text);
+		}
+
+		if (ImGui::InputTextWithHint("Input text", text, textBuffer, 400, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll))
+		{
+			text = textBuffer;
+		}
+	}
+}
+
+void ComponentText::OnLoad(const JSONReader& reader)
+{
+	// Loading input text
+	if (reader.HasMember("Input text"))
+	{
+		const rapidjson::Value& itemText = reader["Input text"];
+		text = itemText.GetString();
+	}
+}
+void ComponentText::OnSave(JSONWriter& writer) const
+{
+	writer.String("Text");
+	writer.StartObject();
+
+	// Saving input text
+	writer.String("Input text");
+	writer.String(text);
+
+	writer.EndObject();
+}
